fb83ca00159c16f5d4904e1f206697c24b624ac6
31ignacio <ahehehinnou31@gmail.com>
2024-03-07 04:57:45 +0100

    commentaire dans le code

--- components/Ampoule.jsx
+++ components/Ampoule.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+ import React from "react";
 import "./Ampoule.css";
 export default function Ampoule({ mode, toggleAmpoule }) {
+  // le mode en parametre c'est pour connaitre l'etat de l'ampoule. Il vient du composant <AmpoulePage/>
+  //toggleAmpoulepour mettre d'allumer ou deteindre sa propre ampoule
   return (
     <div className="single_ampoule_container">
+      {/* Les deux div c'est des conditions sur le 'mode' qui a recu en parametre */}
       <div className={`ampoule_item ${mode ? "light" : "dark"}`}></div>
       <div> Etat:{mode ? "Allumé" : "Eteint"}</div>
 
--- src/AmpoulePage.jsx
+++ src/AmpoulePage.jsx
@@ -2,24 +2,34 @@ import React, { useState } from "react";
 import Ampoule from "../components/Ampoule";
 
 export default function ParentComponent() {
+
+  // Etat qui permet de verifier si tout les ampoules son allumés, eteint par defaut
   const [allAmpouleOn, setAllAmpouleOn] = useState(false);
+  //gestionnaire d'etat pour chaque ampoule de la maison (les ampoules sont allumés par defaut)
   const [ampoulesState, setAmpoulesState] = useState([true, true, true]);
 
+  //la fonction qui permet d'allumer ou d'eteindre toute les ampoules
   const toggleAllAmpoules = () => {
+    //fera le contraire du useState 'allAmpouleOn'
     setAllAmpouleOn(!allAmpouleOn);
+    //parcourir tout les ampoules pour les mettre au meme niveau (il n'aura pas 1 allumé et l'autre eteint)
     const newAmpoulesState = ampoulesState.map(() => allAmpouleOn);
     setAmpoulesState(newAmpoulesState);
   };
 
+  //la fonction qui permet d'allumer ou d'eteindre une ampoule
+  //pour mettre a jour une ampoule on s'est basé sur son index (un peu comme son id)
   const toggleAmpoule = (index) => {
-    const newAmpoulesState = [...ampoulesState];
-    newAmpoulesState[index] = !newAmpoulesState[index];
-    setAmpoulesState(newAmpoulesState);
+    const newAmpoulesState = [...ampoulesState]; //etat de toutes les ampoules existant
+    newAmpoulesState[index] = !newAmpoulesState[index]; //l'etat actuel change (allumé passe a eteint)
+    setAmpoulesState(newAmpoulesState); //la mise a jour
   };
 
   return (
     <>
       <div className="ampoule_container">
+        {/* mode={ampoulesState[0]} ===> pour recuperer l'etat de chaque ampoule */}
+        {/*  toggleAmpoule={() => toggleAmpoule(0)} ===>  permet d'allumer sa propre ampoule ou l'eteindre */}
         <Ampoule
           mode={ampoulesState[0]}
           toggleAmpoule={() => toggleAmpoule(0)}
@@ -35,6 +45,7 @@ export default function ParentComponent() {
       </div>
 
       <div className="main_button_container">
+        {/* le bouton pour allumer ou eteindre toute les ampoules, le css pour la couleur, le 'onclick' qui appel la fonction en haut */}
         <button
           onClick={toggleAllAmpoules}
           className="main_button"
@@ -42,6 +53,8 @@ export default function ParentComponent() {
             backgroundColor: !allAmpouleOn ? "green" : "red",
           }}
         >
+          {/* ceci est une condition */}
+          {/* lorsque les ampoules sont allumé du met 'eteint toutes les ampoules' sinon le contraire */}
           {!allAmpouleOn
             ? "Éteindre toutes les ampoules"
             : "Allumer toutes les ampoules"}
